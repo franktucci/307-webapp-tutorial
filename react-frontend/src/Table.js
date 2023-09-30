a70ae11d5d4326ca6d628c8918c33f85ff51ffe2
franktucci <ftucci@calpoly.edu>
2023-09-29 17:26:44 -0700

    Updated table to instead use Hooks, added delete functionality.

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -4,6 +4,7 @@ function TableHeader() {
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>Remove</th>
       </tr>
       </thead>
   );
@@ -15,6 +16,12 @@ function TableBody(props) {
                 <tr key={index}>
                     <td>{row.name}</td>
                     <td>{row.job}</td>
+                    <td>
+                        <button onClick={() =>
+                            props.removeCharacter(index)}>
+                            Delete
+                        </button>
+                    </td>
                 </tr>
             );
         }
@@ -23,15 +30,17 @@ function TableBody(props) {
         <tbody>
         {rows}
         </tbody>
+
     );
 }
 
 
-function Table(props) {
+function Table (props) {
     return (
         <table>
-            <TableHeader />
-            <TableBody characterData={props.characterData} />
+            <TableHeader/>
+            <TableBody characterData={props.characterData}
+                       removeCharacter={props.removeCharacter}/>
         </table>
     );
 }
